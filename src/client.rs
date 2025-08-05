7640933a3f7082ce27ab52a8fcca518878c1ad30
franknag <49418361+franknag@users.noreply.github.com>
2025-08-05 00:04:53 -0500

    Update client.rs

--- src/client.rs
+++ src/client.rs
@@ -195,6 +195,31 @@ impl Client {
         (Stream, bool, Option<Vec<u8>>, Option<KcpStream>),
         (i32, String),
     )> {
+        // Register mac for local use of wol
+        if let Ok(peerip) = std::net::IpAddr::from_str(peer) {
+            'outer: for peers in hbb_common::config::LanPeers::load().peers {
+                for (ip, mac) in peers.ip_mac.iter() {
+                    if *ip == peer.to_string() {
+                        if let Ok(mac_addr) = mac.parse() {
+                            LocalConfig::set_ip_mac(peer.to_string(), mac.to_string());
+                        }
+                        break 'outer;
+                    }
+                }
+            }
+            let interfaces = default_net::get_interfaces();
+            let wmac = LocalConfig::get_ip_mac(peer);
+            if let Ok(wakemac) = wmac.parse() {
+                for interface in &interfaces {
+                    for ipv4 in &interface.ipv4 {
+                        // remove below mask check to avoid unexpected bug
+                        // if (u32::from(ipv4.addr) & u32::from(ipv4.netmask)) == (u32::from(peer_ip) & u32::from(ipv4.netmask))
+                        log::info!("Send wol to {wakemac} of {}", ipv4.addr);
+                        allow_err!(wol::send_wol(wakemac, None, Some(std::net::IpAddr::V4(ipv4.addr))));
+                    }
+                }
+            }
+        }
         debug_assert!(peer == interface.get_id());
         interface.update_direct(None);
         interface.update_received(false);
