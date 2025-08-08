e6b503004fb0090a70e272c8817ca8cef023bfcb
franknag <49418361+franknag@users.noreply.github.com>
2025-08-08 12:34:07 -0500

    Update lan.rs

--- src/lan.rs
+++ src/lan.rs
@@ -262,10 +262,10 @@ fn wait_response(
 
                             if local_mac.is_empty() && p.mac.is_empty() || local_mac != p.mac {
                                 if hbb_common::config::PeerConfig::exists(&addr.ip().to_string()) { // Custom code for auto create peer mac address wol
-                                    let pconfig = hbb_common::config::PeerConfig::load(&addr.ip().to_string()).clone();
+                                    let mut pconfig = hbb_common::config::PeerConfig::load(&addr.ip().to_string()).clone();
                                 } else {
                                     hbb_common::config::PeerConfig::store(&hbb_common::config::PeerConfig::default(), &addr.ip().to_string());
-                                    let pconfig = hbb_common::config::PeerConfig::load(&addr.ip().to_string()).clone();
+                                    let mut pconfig = hbb_common::config::PeerConfig::load(&addr.ip().to_string()).clone();
                                     pconfig.info.username = p.username.clone();
                                     pconfig.info.hostname = p.hostname.clone();
                                     pconfig.info.platform = p.platform.clone();
